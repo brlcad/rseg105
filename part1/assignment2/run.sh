1fde5ce681a4a51fc426e4e45e243c36af93200f
Sean Morrison <brlcad@gmail.com>
2022-02-18 18:27:23 -0500

    manifest only lists the last one, so use classpath to invoke each main separately

--- part1/assignment2/run.sh
+++ part1/assignment2/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mvn clean install
-java -jar ./target/part1.assignment2-1.0.0.jar edu.morrison.spring.app.BookBeforeAdvice
-java -jar ./target/part1.assignment2-1.0.0.jar edu.morrison.spring.app.BookAfterReturningAdvice
-java -jar ./target/part1.assignment2-1.0.0.jar edu.morrison.spring.app.BookThrowsAdvice
+java -cp ./target/part1.assignment2-1.0.0.jar edu.morrison.spring.app.BookBeforeAdvice
+java -cp ./target/part1.assignment2-1.0.0.jar edu.morrison.spring.app.BookAfterReturningAdvice
+java -cp ./target/part1.assignment2-1.0.0.jar edu.morrison.spring.app.BookThrowsAdvice
