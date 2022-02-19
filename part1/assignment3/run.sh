288c49a338d9cd9980a593d98fcd4e87258654c5
Sean Morrison <brlcad@gmail.com>
2022-02-19 16:10:50 -0500

    pass args to java

--- part1/assignment3/run.sh
+++ part1/assignment3/run.sh
@@ -2,4 +2,4 @@
 
 mvn clean install
 echo "=== Running FindBookCategoryJdbcApp ==========="
-java -jar ./target/part1-assignment3.jar edu.morrison.spring.app.FindBookCategoryJdbcApp
+java -jar ./target/part1-assignment3.jar edu.morrison.spring.app.FindBookCategoryJdbcApp $*
