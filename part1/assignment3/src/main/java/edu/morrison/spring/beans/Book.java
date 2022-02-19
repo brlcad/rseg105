1901f08a1bb5b3734ea4c548bfbc0df3f37e7f1e
Sean Morrison <brlcad@gmail.com>
2022-02-19 12:01:11 -0500

    we were using id for the category, but hold it separately

--- part1/assignment3/src/main/java/edu/morrison/spring/beans/Book.java
+++ part1/assignment3/src/main/java/edu/morrison/spring/beans/Book.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 public class Book implements Serializable {
 
   private Long id = 0L;
+  private Long categoryId = 0L;
   private String isbn = "";
   private String title = "";
   private Float price = 0.0F;
@@ -15,6 +16,13 @@ public class Book implements Serializable {
   }
   public void setId(Long id) {
     this.id = id;
+  }
+
+  public Long getCategoryId() {
+    return categoryId;
+  }
+  public void setCategoryId(Long id) {
+    this.categoryId = id;
   }
 
   public String getIsbn() {
@@ -40,7 +48,7 @@ public class Book implements Serializable {
 
   @Override
   public String toString() {
-    	String s = "Book - Id: " + this.id + ", ISBN: " + this.isbn + ", Title: " + this.title + ", Price: "+ this.price;
+    	String s = "Book - Id: " + this.id + ", Category Id: " + this.categoryId + ", ISBN: " + this.isbn + ", Title: " + this.title + ", Price: "+ this.price;
     	return s;
   }
 
