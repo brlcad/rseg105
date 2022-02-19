2bb267e083f86b552bacb358e625a343898c307d
Sean Morrison <brlcad@gmail.com>
2022-02-18 19:37:27 -0500

    print markers so we can identify them separately

--- part1/assignment2/run.sh
+++ part1/assignment2/run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 mvn clean install
+echo "=== Running BookBeforeAdvice ==========="
 java -cp ./target/part1.assignment2-1.0.0.jar edu.morrison.spring.app.BookBeforeAdvice
+echo "=== Running BookAfterReturningAdvice ==="
 java -cp ./target/part1.assignment2-1.0.0.jar edu.morrison.spring.app.BookAfterReturningAdvice
+echo "=== Running BookThrowsAdvice ==========="
 java -cp ./target/part1.assignment2-1.0.0.jar edu.morrison.spring.app.BookThrowsAdvice
