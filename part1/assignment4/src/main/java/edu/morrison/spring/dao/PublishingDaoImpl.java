8bce1521cea22eed698eda6b5d4eb6548e1d4c1d
Sean Morrison <brlcad@gmail.com>
2022-02-19 21:18:49 -0500

    include comments from what is needed

--- part1/assignment4/src/main/java/edu/morrison/spring/dao/PublishingDaoImpl.java
+++ part1/assignment4/src/main/java/edu/morrison/spring/dao/PublishingDaoImpl.java
@@ -18,5 +18,18 @@ public class PublishingDaoImpl implements PublishingDao, InitializingBean {
       throw new BeanCreationException("Null JdbcTemplate on PublishingDao");
     }
     */
+
+
+    /*
+     * at least one task should be developed with createQuery(…) method
+     *
+     * at least one task should be developed with getNamedQuery(…) without parameter(s).
+     *
+     * at least one task should be developed with getNamedQuery(…) with parameter(s).
+     *
+     * you may if you wish (i.e. not mandatory) to use Hibernate native queries in one of the functionality.
+     *
+     * you develop save and delete functionality using Hibernate.
+     */
   }
 }
