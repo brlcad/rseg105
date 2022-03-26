e6c8061936f7d70dd47ab29bca8ea8ee5b6e85c0
Christopher Sean Morrison <brlcad@gmail.com>
2022-03-26 05:13:40 -0400

    wipe out db here too

--- part2/assignment1/run.sh
+++ part2/assignment1/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+mysql -uroot < src/main/resources/sql/ddl.sql
+mysql -uspring -pspring morebooks < src/main/resources/sql/schema.sql
+mysql -uspring -pspring morebooks < src/main/resources/sql/test-data.sql
+
 mvn clean install
 echo "=== Running PublishingApp ==========="
 java -jar ./target/part2-assignment1.jar edu.morrison.spring.app.PublishingApp $*
