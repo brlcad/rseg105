7aad79f8d9e5ba362fdfede06eecc1af1d39be94
Christopher Sean Morrison <brlcad@gmail.com>
2022-03-26 13:56:57 -0400

    lets start with just findall

--- part2/assignment2/src/main/java/edu/morrison/spring/app/PublishingApp.java
+++ part2/assignment2/src/main/java/edu/morrison/spring/app/PublishingApp.java
@@ -12,7 +12,6 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 import edu.morrison.spring.beans.Author;
 import edu.morrison.spring.beans.Book;
 import edu.morrison.spring.beans.Category;
-import edu.morrison.spring.service.BookService;
 
 
 public class PublishingApp {
@@ -25,7 +24,7 @@ public class PublishingApp {
 
     ctx.load("classpath:spring/app-context-annotation.xml");
     ctx.refresh();
-    bookService = ctx.getBean(BookService.class);
+    bookService = ctx.getBean("springJpaBookService", BookService.class);
 
     Integer demo = 0; /* default is all of them */
 
@@ -34,7 +33,7 @@ public class PublishingApp {
     logger.info("");
     logger.info("Usage: " + args[0] + "[1234]");
     logger.info("");
-    logger.info("This is a demonstration of persisting entities via Spring JPA2.");
+    logger.info("This is a demonstration of persisting entities via Spring Data JPA2.");
     logger.info("Specify number to run each specific demo in succession (allowing database inspection).");
     logger.info("");
 
@@ -48,56 +47,54 @@ public class PublishingApp {
 
     logger.info("============== Start of Publishing Demo =====================");
 
-    if (demo == 0 || demo == 1)
+    /*    if (demo == 0 || demo == 1)
       demoLookupBook();
+    */
     if (demo == 0 || demo == 2)
-      demoCreateBook();
-    if (demo == 0 || demo == 3)
-      demoDeleteBook();
-    if (demo == 0 || demo == 4)
       demoFindAllBooks();
-
+    /*
+    if (demo == 0 || demo == 3)
+      demoCreateBook();
+    if (demo == 0 || demo == 4)
+      demoDeleteBook();
+    */
     logger.info("============== End of Publishing Demo =======================");
 
   }
 
 
+  private static void printBook(Book book) {
+    logger.info(book.toString());
+    Category category = book.getCategory();
+    if (category != null)
+      logger.info(category.toString());
+    if (book.getAuthors() != null) {
+      book.getAuthors().forEach(i -> logger.info(i.toString()));
+    }
+  }
+
+
+  /*
+  private static void demoLookupBook() {
+    logger.info("------- Demo 1: Find a book by id");
+
+    Long id = 9L;
+    Book bookById = bookService.findBookWithAuthorAndCategoryById(id);
+    printBook(bookById);
+  }
+  */
+
   private static void demoFindAllBooks() {
-    logger.info("------- Demo 4: Find all books for one author id who has more than one book");
+    logger.info("------- Demo 2: Find all books without details");
 
     /* author #1 has multiple books */
-    logger.info("  listing books by author #1...");
-    Long id = 1L;
-    List<Book> books = bookService.findAllBooksByAuthorId(id);
+    List<Book> books = bookService.findAll();
     for(Book b : books) {
       printBook(b);
     }
-    logger.info("  ... end listing of books by author #1");
   }
 
-
-  private static void demoDeleteBook() {
-    logger.info("------- Demo 3: Delete a saved book and author(s) from the database");
-
-    Long id = 10L;
-    Book bookById = bookService.findBookWithAuthorAndCategoryById(id);
-    if (bookById != null) {
-      logger.info("  found book #10...");
-      printBook(bookById);
-      bookService.delete(bookById);
-    } else {
-      return;
-    }
-
-    bookById = bookService.findBookWithAuthorAndCategoryById(id);
-    if (bookById == null) {
-      logger.info("  ...successfully deleted book #10");
-    } else {
-      logger.info("  ...unsuccessfully deleted book #10");
-    }
-  }
-
-
+  /*
   private static void demoCreateBook() {
     logger.info("------- Demo 2: Create a new book with a new author(s)");
 
@@ -125,21 +122,25 @@ public class PublishingApp {
   }
 
 
-  private static void demoLookupBook() {
-    logger.info("------- Demo 1: Find a book by id");
+  private static void demoDeleteBook() {
+    logger.info("------- Demo 3: Delete a saved book and author(s) from the database");
 
-    Long id = 9L;
+    Long id = 10L;
     Book bookById = bookService.findBookWithAuthorAndCategoryById(id);
-    printBook(bookById);
-  }
+    if (bookById != null) {
+      logger.info("  found book #10...");
+      printBook(bookById);
+      bookService.delete(bookById);
+    } else {
+      return;
+    }
 
-  private static void printBook(Book book) {
-    logger.info(book.toString());
-    Category category = book.getCategory();
-    if (category != null)
-      logger.info(category.toString());
-    if (book.getAuthors() != null) {
-      book.getAuthors().forEach(i -> logger.info(i.toString()));
+    bookById = bookService.findBookWithAuthorAndCategoryById(id);
+    if (bookById == null) {
+      logger.info("  ...successfully deleted book #10");
+    } else {
+      logger.info("  ...unsuccessfully deleted book #10");
     }
   }
+  */
 }
