836f2ab4079e0ee88b783184fda4e00aa26bd793
Christopher Sean Morrison <brlcad@gmail.com>
2022-03-26 23:39:59 -0400

    no longer running the jar, just installing the war

--- part2/assignment3/run.sh
+++ part2/assignment3/run.sh
@@ -6,4 +6,5 @@ mysql -uspring -pspring mvcbooks < src/main/resources/sql/test-data.sql
 
 mvn clean install
 echo "=== Running BookWebApp ==========="
-java -jar ./target/part2-assignment3.jar edu.morrison.spring.BookWebApp $*
+
+# java -jar ./target/part2-assignment3.jar edu.morrison.spring.BookWebApp $*
